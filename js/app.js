4b7efc3856816ab2de4c267de0ccb1bdfefc32ed
Sergio <lopecitobmx@gmail.com>
2018-04-09 05:32:43 +0200

    Delete given comments and increase readability

--- js/app.js
+++ js/app.js
@@ -1,8 +1,3 @@
-/*
- * Create a list that holds all of your cards
- */
-
-
 let card = $(".card");
 
 let cards = [...card];
@@ -18,8 +13,11 @@ let stars = $(".fa-star");
 let matchedCards = [];
 
 let seconds, minutes = 0;
+
 let timer = $(".timer");
+
 timer.html("0mins 0secs");
+
 var interval;
 
 let pickedCards = [];
@@ -27,17 +25,8 @@ let pickedCards = [];
 let clicks = [];
 
 
-
 $(document).ready(gameOn());
 
-
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -56,62 +45,60 @@ function shuffle(array) {
 
 
 
-function gameOn(){
+function gameOn() {
+    cards = $(shuffle(cards));
 
-  cards = $(shuffle(cards));
+    deck.html("");
 
-  deck.html("");
-
-  cards.each(function(){
-    deck.append(this);
-    $(this).removeClass("show open match disabled");
-  });
-
-  plays = 0;
-
-  counter.html(plays);
-
-  stars.each(function(){
-    $(this).css({
-      "color" : "#faff00",
-      "visibility" : "visible"
+    cards.each(function() {
+        deck.append(this);
+        $(this).removeClass("show open match disabled");
       });
-    });
 
-  seconds = 0;
-  minutes = 0;
-  timer.html("0mins 0secs")
-  clearInterval(interval);
+    plays = 0;
 
+    counter.html(plays);
+
+    stars.each(function() {
+        $(this).css({
+                    "color" : "#faff00",
+                    "visibility" : "visible"
+                  });
+                });
+
+    seconds = 0;
+    minutes = 0;
+    timer.html("0mins 0secs")
+    clearInterval(interval);
 }
 
-
-function timerOn(){
-    interval = setInterval(function(){
-        timer.html(minutes+"mins "+seconds+"secs") ;
+function timerOn() {
+    interval = setInterval(function() {
+        timer.html(minutes+"mins "+seconds+"secs");
         seconds++;
-        if(seconds == 60){
+        if(seconds == 60) {
             minutes++;
             seconds=0;
         }
     },1000);
 }
 
-let showCard = function(){
-  clicks.push(this);
-  if (clicks.length === 1){
-    timerOn();
- }
+let showCard = function() {
+    clicks.push(this);
 
-  $(this).toggleClass("open show disabled");
+    if (clicks.length === 1){
+        timerOn();
+    };
+
+    $(this).toggleClass("open show disabled");
 }
 
-function openCard(){
-
+function openCard() {
   pickedCards.push(this);
+
   let long = pickedCards.length;
 
-  if (long === 2){
+  if (long === 2) {
     playsCounter();
     if (pickedCards[0].type === pickedCards[1].type){
       matchCards();
@@ -121,106 +108,95 @@ function openCard(){
   }
 }
 
-function playsCounter(){
-  plays++;
-  counter.html(plays)
+function playsCounter() {
+    plays++;
+    counter.html(plays)
 
-  if (plays > 9 && plays <= 13){
+    if (plays > 10 && plays <= 15) {
         $(".fa-star:last").css("visibility", "collapse");
 
-  } else if (plays > 13){
+    } else if (plays > 15) {
         $(".middle").css("visibility", "collapse");
-    }
-
+      };
 }
 
-function matchCards(){
-  $(pickedCards[0]).removeClass("show open").addClass("match disabled");
-  $(pickedCards[1]).removeClass("show open").addClass("match disabled");
-  matchedCards.push(pickedCards[0]);
-  matchedCards.push(pickedCards[1]);
-  pickedCards = [];
-}
-
-function unmatchCards(){
-  $(pickedCards[0]).addClass("unmatch");
-  $(pickedCards[1]).addClass("unmatch");
-  disable();
-  setTimeout(function(){
-    $(pickedCards[0]).removeClass("show open disabled unmatch");
-    $(pickedCards[1]).removeClass("show open disabled unmatch");
-    enable();
+function matchCards() {
+    $(pickedCards[0]).removeClass("show open").addClass("match disabled");
+    $(pickedCards[1]).removeClass("show open").addClass("match disabled");
+    matchedCards.push(pickedCards[0]);
+    matchedCards.push(pickedCards[1]);
     pickedCards = [];
-  }, 750);
 }
 
-function disable(){
-    Array.prototype.filter.call(cards, function(){
+function unmatchCards() {
+    $(pickedCards[0]).addClass("unmatch");
+    $(pickedCards[1]).addClass("unmatch");
+
+    blockCard();
+
+    setTimeout(function() {
+        $(pickedCards[0]).removeClass("show open disabled unmatch");
+        $(pickedCards[1]).removeClass("show open disabled unmatch");
+        unblockCard();
+        pickedCards = [];
+      }, 750);
+}
+
+function blockCard() {
+    Array.prototype.filter.call(cards, function() {
         card.addClass("disabled");
     });
 }
 
-function enable(){
-    Array.prototype.filter.call(cards, function(){
+function unblockCard() {
+    Array.prototype.filter.call(cards, function() {
         card.removeClass("disabled");
-        $(".match").each(function(){
-          $(this).addClass("disabled");
-        });
+
+        $(".match").each(function() {
+            $(this).addClass("disabled");
+          });
       });
-  }
+}
 
+function gameOver() {
+    if (matchedCards.length === 16) {
+        clearInterval(interval);
+        let timeMade = timer.html();
+        let scoreMade = $(".stars").html();
+        $(".modal").css("display", "block");
 
-function gameOver(){
-  if (matchedCards.length === 16){
-    clearInterval(interval);
-    let timeMade = timer.html();
-    let scoreMade = $(".stars").html();
-    $(".modal").css("display", "block");
+        if ($(".fa-star:last").css("visibility") === "collapse" && $(".middle").css("visibility") === "collapse" ) {
+            $(".message").html("You did it, but I guess you can do it better than that.");
 
-    if ($(".fa-star:last").css("visibility") === "collapse" && $(".middle").css("visibility") === "collapse" ){
-      $(".message").html("You did it, but I guess you can do it better than that.");
-    } else if ($(".fa-star:last").css("visibility") === "collapse"){
-        $(".message").html("Well done!!! Do you dare to go for a perfect score?");
-    } else {
-        $(".message").html("Amazing. You've rated the highest score");
-    }
+        } else if ($(".fa-star:last").css("visibility") === "collapse") {
+            $(".message").html("Well done!!! Do you dare to go for a perfect score?");
 
-    $(".playsMade").html(plays);
-    $(".timeMade").html(timeMade);
-    $(".scoreMade").html(scoreMade);
+        } else {
+            $(".message").html("Amazing. You've rated the highest score");
+          };
 
-    playAgain();
-  };
+        $(".playsMade").html(plays);
+        $(".timeMade").html(timeMade);
+        $(".scoreMade").html(scoreMade);
 
+        playAgain();
+    };
 }
 
 function playAgain() {
-  $(".modalRestart").click(function(){
-    $(".modal").css("display", "none");
-    matchedCards = [];
-    location.reload();
-  });
-
+  $(".modalRestart").click(function() {
+      $(".modal").css("display", "none");
+      matchedCards = [];
+      location.reload();
+    });
 }
 
-$(".restart").click(function(){
+$(".restart").click(function() {
   location.reload();
 })
 
-cards.each(function(){
+cards.each(function() {
   $(this).click(showCard);
   $(this).click(openCard);
   $(this).click(gameOver);
-
 })
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
